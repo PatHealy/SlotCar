b808353f31d1fa516a0d8bb8c9bc2c8b8617a256
PatHealy <pwh5@pitt.edu>
2019-01-26 23:28:04 -0500

    Dog follow car

---
+++ SlotCar/Assets/_Scripts/TubsBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TubsBehavior : MonoBehaviour {
+    NavMeshAgent ai;
+    public Transform dest;
+	// Use this for initialization
+	void Start () {
+        ai = gameObject.GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ai.destination = dest.position;
+	}
+}
